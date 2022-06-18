98ff3c9fb58d24c16af12b9b25d7f1aa753fe64e
chuang2 <chuang2@ps004.is.localnet>
2022-06-18 18:30:46 +0200

    correct BSTRO arch

--- metro/tools/demo_bstro.py
+++ metro/tools/demo_bstro.py
@@ -10,11 +10,17 @@ from __future__ import absolute_import, division, print_function
 import argparse
 import os
 import torch
+import torchvision.models as models
 from torchvision.utils import make_grid
 import torchvision.transforms as transforms
 import numpy as np
 import cv2
+from metro.modeling.bert import BertConfig, METRO
+from metro.modeling.bert.modeling_bstro import BSTRO_BodyHSC_Network as BSTRO_Network
 from metro.modeling._smpl import SMPL, Mesh
+from metro.modeling.hrnet.hrnet_cls_net_featmaps import get_cls_net
+from metro.modeling.hrnet.config import config as hrnet_config
+from metro.modeling.hrnet.config import update_config as hrnet_update_config
 from metro.utils.image_ops import crop
 from metro.utils.logger import setup_logger
 from metro.utils.comm import synchronize, is_main_process, get_rank
@@ -32,16 +38,16 @@ def rgb_processing(rgb_img, center, scale, rot, pn, img_res=224):
     rgb_img = np.transpose(rgb_img.astype('float32'),(2,0,1))/255.0
     return rgb_img
 
-def run_inference(args, METRO_model, smpl, mesh_sampler):
+def run_inference(args, BSTRO_model, smpl, mesh_sampler):
     smpl.eval()
 
     if args.distributed:
-        METRO_model = torch.nn.parallel.DistributedDataParallel(
-            METRO_model, device_ids=[args.local_rank], 
+        BSTRO_model = torch.nn.parallel.DistributedDataParallel(
+            BSTRO_model, device_ids=[args.local_rank], 
             output_device=args.local_rank,
             find_unused_parameters=True,
         )
-    METRO_model.eval()
+    BSTRO_model.eval()
 
     normalize_img = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
@@ -66,10 +72,10 @@ def run_inference(args, METRO_model, smpl, mesh_sampler):
         images = images.cuda(args.device).unsqueeze(0)
 
         # forward-pass
-        if METRO_model.config.output_attentions:
-            _, _, pred_contact, hidden_states, att = METRO_model(images, smpl, mesh_sampler)
+        if BSTRO_model.config.output_attentions:
+            _, _, pred_contact, hidden_states, att = BSTRO_model(images, smpl, mesh_sampler)
         else:
-            _, _, pred_contact = METRO_model(images, smpl, mesh_sampler)
+            _, _, pred_contact = BSTRO_model(images, smpl, mesh_sampler)
 
         
         visual_imgs, pred_contact_meshes = visualize_contact([img],
@@ -83,11 +89,11 @@ def run_inference(args, METRO_model, smpl, mesh_sampler):
             foldername = './demo'
             if not os.path.exists(foldername):
                 os.makedirs(foldername)
-            temp_fname = foldername + '/visual.jpg'
+            temp_fname = foldername + '/input.jpg'
             cv2.imwrite(temp_fname, np.asarray(visual_imgs[:,:,::-1]*255))
 
-            for mi, mesh in enumerate(pred_contact_meshes):
-                temp_fname = foldername + f'/contact_{mi}.obj'
+            for _, mesh in enumerate(pred_contact_meshes):
+                temp_fname = foldername + f'/contact_vis.obj'
                 mesh.export(temp_fname)
     return
 
@@ -122,6 +128,25 @@ def visualize_contact(images,
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    #########################################################
+    # Model architectures
+    #########################################################
+    parser.add_argument('-a', '--arch', default='hrnet-w64',
+                    help='CNN backbone architecture: hrnet-w64, hrnet, resnet50')
+    parser.add_argument("--num_hidden_layers", default=4, type=int, required=False, 
+                        help="Update model config if given")
+    parser.add_argument("--hidden_size", default=-1, type=int, required=False, 
+                        help="Update model config if given")
+    parser.add_argument("--num_attention_heads", default=4, type=int, required=False, 
+                        help="Update model config if given. Note that the division of "
+                        "hidden_size / num_attention_heads should be in integer.")
+    parser.add_argument("--intermediate_size", default=-1, type=int, required=False, 
+                        help="Update model config if given.")
+    parser.add_argument("--input_feat_dim", default='2051,512,128', type=str, 
+                        help="The Image Feature Dimension.")          
+    parser.add_argument("--hidden_feat_dim", default='1024,256,128', type=str, 
+                        help="The Image Feature Dimension.")   
+    parser.add_argument("--legacy_setting", default=True, action='store_true',)
     
     #########################################################
     # Loading/saving checkpoints
@@ -132,7 +157,10 @@ def parse_args():
                         help="The path to the input image.")
     parser.add_argument("--output_dir", default='output/', type=str, required=False,
                         help="The output directory to save checkpoint and test results.")
-    
+    parser.add_argument("--model_name_or_path", default='metro/modeling/bert/bert-base-uncased/', type=str, required=False,
+                        help="Path to pre-trained transformer model or model type.")
+    parser.add_argument("--config_name", default="", type=str, 
+                        help="Pretrained config name or path if not the same as model_name.")
     #########################################################
     # Others
     #########################################################
@@ -163,7 +191,7 @@ def main(args):
         synchronize()
 
     mkdir(args.output_dir)
-    logger = setup_logger("METRO", args.output_dir, get_rank())
+    logger = setup_logger("BSTRO", args.output_dir, get_rank())
     # set_seed(args.seed, args.num_gpus)
     logger.info("Using {} GPUs".format(args.num_gpus))
 
@@ -171,15 +199,90 @@ def main(args):
     smpl = SMPL().to(args.device)
     mesh_sampler = Mesh()
 
-    if args.resume_checkpoint!=None and args.resume_checkpoint!='None' and 'state_dict' not in args.resume_checkpoint:
+    # Load model
+    trans_encoder = []
+
+    input_feat_dim = [int(item) for item in args.input_feat_dim.split(',')]
+    hidden_feat_dim = [int(item) for item in args.hidden_feat_dim.split(',')]
+    output_feat_dim = input_feat_dim[1:] + [1]
+
+    for i in range(len(output_feat_dim)):
+        config_class, model_class = BertConfig, METRO
+        config = config_class.from_pretrained(args.config_name if args.config_name \
+                else args.model_name_or_path)
+
+        config.output_attentions = args.output_attentions
+        config.hidden_dropout_prob = 0.1
+        config.img_feature_dim = input_feat_dim[i] 
+        config.output_feature_dim = output_feat_dim[i]
+        args.hidden_size = hidden_feat_dim[i]
+
+        if args.legacy_setting==True:
+            # During our paper submission, we were using the original intermediate size, which is 3072 fixed
+            # We keep our legacy setting here 
+            args.intermediate_size = -1
+        else:
+            # We have recently tried to use an updated intermediate size, which is 4*hidden-size.
+            # But we didn't find significant performance changes on Human3.6M (~36.7 PA-MPJPE)
+            args.intermediate_size = int(args.hidden_size*4)
+
+        # update model structure if specified in arguments
+        update_params = ['num_hidden_layers', 'hidden_size', 'num_attention_heads', 'intermediate_size']
+
+        for idx, param in enumerate(update_params):
+            arg_param = getattr(args, param)
+            config_param = getattr(config, param)
+            if arg_param > 0 and arg_param != config_param:
+                logger.info("Update config parameter {}: {} -> {}".format(param, config_param, arg_param))
+                setattr(config, param, arg_param)
+
+        # init a transformer encoder and append it to a list
+        assert config.hidden_size % config.num_attention_heads == 0
+        model = model_class(config=config) 
+        logger.info("Init model from scratch.")
+        trans_encoder.append(model)
+
+    
+    # init ImageNet pre-trained backbone model
+    if args.arch=='hrnet':
+        hrnet_yaml = 'models/hrnet/cls_hrnet_w40_sgd_lr5e-2_wd1e-4_bs32_x100.yaml'
+        hrnet_checkpoint = 'models/hrnet/hrnetv2_w40_imagenet_pretrained.pth'
+        hrnet_update_config(hrnet_config, hrnet_yaml)
+        backbone = get_cls_net(hrnet_config, pretrained=hrnet_checkpoint)
+        logger.info('=> loading hrnet-v2-w40 model')
+    elif args.arch=='hrnet-w64':
+        hrnet_yaml = 'models/hrnet/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml'
+        hrnet_checkpoint = 'models/hrnet/hrnetv2_w64_imagenet_pretrained.pth'
+        hrnet_update_config(hrnet_config, hrnet_yaml)
+        backbone = get_cls_net(hrnet_config, pretrained=hrnet_checkpoint)
+        logger.info('=> loading hrnet-v2-w64 model')
+    else:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        backbone = models.__dict__[args.arch](pretrained=True)
+        # remove the last fc layer
+        backbone = torch.nn.Sequential(*list(backbone.children())[:-2])
+
+    trans_encoder = torch.nn.Sequential(*trans_encoder)
+    total_params = sum(p.numel() for p in trans_encoder.parameters())
+    logger.info('Transformers total parameters: {}'.format(total_params))
+    backbone_total_params = sum(p.numel() for p in backbone.parameters())
+    logger.info('Backbone total parameters: {}'.format(backbone_total_params))
+
+    # build end-to-end METRO network (CNN backbone + multi-layer transformer encoder)
+    _bstro_network = BSTRO_Network(args, config, backbone, trans_encoder, mesh_sampler)
+
+    if args.resume_checkpoint!=None and args.resume_checkpoint!='None':# and 'state_dict' not in args.resume_checkpoint:
         # if only run eval, load checkpoint
         logger.info("Evaluation: Loading from checkpoint {}".format(args.resume_checkpoint))
-        _metro_network = torch.load(args.resume_checkpoint)
+        cpu_device = torch.device('cpu')
+        state_dict = torch.load(args.resume_checkpoint, map_location=cpu_device)
+        _bstro_network.load_state_dict(state_dict, strict=False)
+        del state_dict
     else:
         raise ValueError("Invalid checkpoint {}".format(args.resume_checkpoint))
     
-    _metro_network.to(args.device)
-    run_inference(args, _metro_network, smpl, mesh_sampler)
+    _bstro_network.to(args.device)
+    run_inference(args, _bstro_network, smpl, mesh_sampler)
 
    
 
