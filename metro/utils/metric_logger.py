46e65450f29d17313d5c7a41ff8283aecd610e75
chuang2 <chuang2@ps004.is.localnet>
2022-07-02 12:07:48 +0200

    update license

--- metro/utils/metric_logger.py
+++ metro/utils/metric_logger.py
@@ -1,8 +1,19 @@
 """
-Copyright (c) Microsoft Corporation.
-Licensed under the MIT license.
+Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
+holder of all proprietary rights on this computer program.
+You can only use this computer program if you have closed
+a license agreement with MPG or you get the right to use the computer
+program from someone who is authorized to grant you that right.
+Any use of the computer program without a valid license is prohibited and
+liable to prosecution.
 
-Basic logger. It Computes and stores the average and current value
+Copyright©2022 Max-Planck-Gesellschaft zur Förderung
+der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
+for Intelligent Systems and the Max Planck Institute for Biological
+Cybernetics. All rights reserved.
+
+Contact: ps-license@tuebingen.mpg.de
+
 """
 
 class AverageMeter(object):
